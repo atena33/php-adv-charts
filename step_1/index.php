c1b0c34986e68da058b86701b2cd15c39b76ea6a
Anna <annamura833@gmail.com>
2020-05-11 12:13:44 +0200

    Primo step

---
+++ step_1/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@2/dist/Chart.min.js"></script>
+    <script src="main.js"></script>
+    <title>Charts</title>
+  </head>
+  <body>
+    <main>
+      <div class="container">
+        <canvas id="canvas"></canvas>
+      </div>
+    </main>
+  </body>
+</html>
